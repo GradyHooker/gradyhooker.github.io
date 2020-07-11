c55443e3c22089ea49547a812447aa377031898e
GradyHooker <bloodninja28@gmail.com>
2020-07-11 22:13:32 +1200

    Fix GA

--- js/script.js
+++ js/script.js
@@ -1,129 +1,138 @@
 $(function() {
-	$("html").click(hideSidePanel);
-	
-	$("#sidePanel").click(function() {
-		event.stopPropagation();
+	window.addEventListener('hashchange', function() {
+		buildSite();
 	});
+	buildSite();
 	
-	var hash;
-	if(location.hash == null || location.hash == "") {
-		hash = "projects";
-	} else {
-		hash = location.hash.substr(1);
-	}
+	function buildSite() {
+		//Start with a Google Analytics view
+		ga('create', 'UA-123516744-1', 'auto');
+		ga('send', 'pageview', location.pathname + location.hash);
+		
+		//Then build the page
+		var hash;
+		if(location.hash == null || location.hash == "") {
+			hash = "projects";
+		} else {
+			hash = location.hash.substr(1);
+		}
 	
-	if(jsonLabels[hash] != null && jsonLabels[hash] != "") {
-		//Do the thing
-		$('#content-name').text(jsonLabels[hash]);
-		jsonBoxes.forEach(function(box) {
-			if(box.categories.includes(hash)) {
-				//Make outer element
-				if(box.categories.includes('media')) {
-					//Structure like Media		
-					d = document.createElement('div');
-					$(d).addClass('media-cont');
-					box.categories.forEach(function(className) {
-						$(d).addClass(className);
-					});		
-					$(d).click(function(){ showMediaInfo(box.name); });
-					
-					f = document.createElement('div');
-					$(f).addClass('media-logo-cont');
-					$(d).append(f);
-					sc = document.createElement('div');
-					$(sc).addClass('media-logo');
-					$(sc).html('<img class="media-logo-img" src="images/logos_white/' + box.logo + '" alt="' + box.title + ' Image"/>');
-					$(f).append(sc);
-					
-					e = document.createElement('div');
-					$(e).addClass('media-text-cont');
-					$(d).append(e);
-					
-					title = document.createElement('div');
-					$(title).addClass('media-title');
-					$(title).attr('data-date', box.date);
-					$(title).text(box.title);
-					$(e).append(title);
-					
-					country = document.createElement('div');
-					$(country).addClass('media-text');
-					$(country).html(box.date + ', ' + box.country.city + ' <img src="images/blank.gif" class="flag flag-' + box.country.code + '" alt="' + box.country.text + '" />');
-					$(e).append(country);
-					
-				} else if(box.categories.includes('work') || box.categories.includes('projects') || box.categories.includes('accomplishments')) {
-					//Structure like Work Experience or Project or Accomplishment
-					d = document.createElement('div');
-					$(d).addClass('project-cont');
-					box.categories.forEach(function(className) {
-						$(d).addClass(className);
-					});
-				
-					//Make Screenshot
-					sc = document.createElement('div');
-					$(sc).addClass('project-screenshot-cont');
-					$(sc).html('<img class="project-screenshot" src="images/' + box.screenshot + '" alt="' + box.title + ' Image"/>');
-					$(d).append(sc);
-					
-					//Make Project Logo
-					logo = document.createElement('div');
-					$(logo).addClass('project-logo-cont');
-					if(box.logoLink != null && box.logoLink != "") {
-						$(logo).html('<a href="' + box.logoLink + '" target="_blank"><img class="project-logo" src="images/' + box.logo + '" alt="' + box.title + ' Logo"/></a>');
-					} else {
-						$(logo).html('<img class="project-logo" src="images/' + box.logo + '" alt="' + box.title + ' Logo"/>');
-					}
-					$(d).append(logo);
-					
-					//Make Project Links
-					projectLinks = document.createElement('div');
-					$(projectLinks).addClass('project-links');
-					box.links.forEach(function(link) {
-						url = document.createElement('a');
-						url.target = "_blank";
-						url.href = link.link;
-						$(url).html('<img class="project-icon" src="icons/' + link.image + '" alt="' + link.text + '" title="' + link.text + '"/>');
-						$(projectLinks).append(url);
-					});
-					$(d).append(projectLinks);
-					
-					if(box.categories.includes('work')) {
-						//Make Project Title 
-						title = document.createElement('div');
-						$(title).addClass('project-title');
-						$(title).html(box.role + "<br/>(" + box.title + ")");
-						$(d).append(title);
-						
-						//Make Project Years
-						years = document.createElement('div');
-						$(years).addClass('project-years');
-						$(years).text(box.years);
-						$(d).append(years);
-					} else {
-						//Make Project Title 
-						title = document.createElement('div');
-						$(title).addClass('project-title');
-						$(title).text(box.title);
-						$(d).append(title);
-					}
-					
-					//Make Project Description
-					description = document.createElement('div');
-					$(description).addClass('project-description');
-					$(description).text(box.description);
-					$(d).append(description);
-				}
-				
-				$('#content').append(d);
-			}
+		$("html").click(hideSidePanel);
+		
+		$("#sidePanel").click(function() {
+			event.stopPropagation();
 		});
-	} else {
-		$('#content-name').text("Unrecognized URL");
+		
+		$('#content').empty();
+			
+		if(jsonLabels[hash] != null && jsonLabels[hash] != "") {
+			//Do the thing
+			$('#content-name').text(jsonLabels[hash]);
+			jsonBoxes.forEach(function(box) {
+				if(box.categories.includes(hash)) {
+					//Make outer element
+					if(box.categories.includes('media')) {
+						//Structure like Media		
+						d = document.createElement('div');
+						$(d).addClass('media-cont');
+						box.categories.forEach(function(className) {
+							$(d).addClass(className);
+						});		
+						$(d).click(function(){ showMediaInfo(box.name); });
+						
+						f = document.createElement('div');
+						$(f).addClass('media-logo-cont');
+						$(d).append(f);
+						sc = document.createElement('div');
+						$(sc).addClass('media-logo');
+						$(sc).html('<img class="media-logo-img" src="images/logos_white/' + box.logo + '" alt="' + box.title + ' Image"/>');
+						$(f).append(sc);
+						
+						e = document.createElement('div');
+						$(e).addClass('media-text-cont');
+						$(d).append(e);
+						
+						title = document.createElement('div');
+						$(title).addClass('media-title');
+						$(title).attr('data-date', box.date);
+						$(title).text(box.title);
+						$(e).append(title);
+						
+						country = document.createElement('div');
+						$(country).addClass('media-text');
+						$(country).html(box.date + ', ' + box.country.city + ' <img src="images/blank.gif" class="flag flag-' + box.country.code + '" alt="' + box.country.text + '" />');
+						$(e).append(country);
+						
+					} else if(box.categories.includes('work') || box.categories.includes('projects') || box.categories.includes('accomplishments')) {
+						//Structure like Work Experience or Project or Accomplishment
+						d = document.createElement('div');
+						$(d).addClass('project-cont');
+						box.categories.forEach(function(className) {
+							$(d).addClass(className);
+						});
+					
+						//Make Screenshot
+						sc = document.createElement('div');
+						$(sc).addClass('project-screenshot-cont');
+						$(sc).html('<img class="project-screenshot" src="images/' + box.screenshot + '" alt="' + box.title + ' Image"/>');
+						$(d).append(sc);
+						
+						//Make Project Logo
+						logo = document.createElement('div');
+						$(logo).addClass('project-logo-cont');
+						if(box.logoLink != null && box.logoLink != "") {
+							$(logo).html('<a href="' + box.logoLink + '" target="_blank"><img class="project-logo" src="images/' + box.logo + '" alt="' + box.title + ' Logo"/></a>');
+						} else {
+							$(logo).html('<img class="project-logo" src="images/' + box.logo + '" alt="' + box.title + ' Logo"/>');
+						}
+						$(d).append(logo);
+						
+						//Make Project Links
+						projectLinks = document.createElement('div');
+						$(projectLinks).addClass('project-links');
+						box.links.forEach(function(link) {
+							url = document.createElement('a');
+							url.target = "_blank";
+							url.href = link.link;
+							$(url).html('<img class="project-icon" src="icons/' + link.image + '" alt="' + link.text + '" title="' + link.text + '"/>');
+							$(projectLinks).append(url);
+						});
+						$(d).append(projectLinks);
+						
+						if(box.categories.includes('work')) {
+							//Make Project Title 
+							title = document.createElement('div');
+							$(title).addClass('project-title');
+							$(title).html(box.role + "<br/>(" + box.title + ")");
+							$(d).append(title);
+							
+							//Make Project Years
+							years = document.createElement('div');
+							$(years).addClass('project-years');
+							$(years).text(box.years);
+							$(d).append(years);
+						} else {
+							//Make Project Title 
+							title = document.createElement('div');
+							$(title).addClass('project-title');
+							$(title).text(box.title);
+							$(d).append(title);
+						}
+						
+						//Make Project Description
+						description = document.createElement('div');
+						$(description).addClass('project-description');
+						$(description).text(box.description);
+						$(d).append(description);
+					}
+					
+					$('#content').append(d);
+				}
+			});
+		} else {
+			$('#content-name').text("Unrecognized URL");
+		}
 	}
-	
-	$('header a').click(function() {
-		window.location.href = this.href;
-		location.reload();
-	});
 	
 	//Set the Sticky Image
 	var gradyImages = [
